fbdbeadc37e8c3ab5597eee04f522a26ae052980
leesr <leesr022256@gmail.com>
2025-03-05 13:27:53 +0900

    updown counter 코드입니다다

---
+++ test.v
@@ -0,0 +1,25 @@
+////////////////////////////////////////////////////////////////////////////
+// Filename       : ud_cnt                                        
+// Date           : 2024. 04. 08                                           
+//                                                                        
+// Author         : mkkim                                                   
+// Abstract       :                                                       
+////////////////////////////////////////////////////////////////////////////
+module ud_cnt(i_clk, i_rstn, o_cnt, i_ud);
+
+	input i_clk, i_rstn, i_ud;
+	output reg[15:0] o_cnt;
+
+	always @ (posedge i_clk or negedge i_rstn) begin
+		if (!i_rstn) begin
+			o_cnt	<= 0;
+		end
+		else begin
+			if (i_ud == 1'b1)
+				o_cnt	<= o_cnt + 16'b1;
+			else
+				o_cnt	<= o_cnt - 16'b1;
+		end
+	end
+endmodule
+
